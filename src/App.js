e210be78d8d18477dba85bdec3ad1b686ee130ec
felixongati <fegati02@gmail.com>
2019-08-26 18:59:02 +0300

    first class component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       <Greet/>
+      <Welcome/>
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello Fegati</h1>
-}
-
+// function Greet() {
+//     return <h1>Hello Fegati</h1>
+// }
+const Greet = () => <h1>Hello Fegati</h1>
 export default Greet
