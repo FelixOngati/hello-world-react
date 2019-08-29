5a5ea33977a2650e653a7fe4ace39bc35360c384
felixongati <fegati02@gmail.com>
2019-08-29 20:31:40 +0300

    set state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+      <Counter/>
       {/*<Greet name='Vishwas' heroName='Batman'>*/}
       {/*  <p>This is a paragraph</p>*/}
       {/*</Greet>*/}
