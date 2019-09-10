c241a56c82f0a0c4d1cd5c493e5852f11015f6c6
felixongati <fegati02@gmail.com>
2019-09-10 10:47:19 +0300

    binding event handlers

--- src/App.js
+++ src/App.js
@@ -6,22 +6,28 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick"
+import ClassClick from "./components/ClassClick"
+import EventBind from "./components/EventBind"
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      <Greet name='Vishwas' heroName='Batman'>
+        <EventBind/>
+        {/*<FunctionClick/>*/}
+        {/*<ClassClick/>*/}
+      {/*<Counter/>*/}
+      {/*<Greet name='Vishwas' heroName='Batman'>*/}
       {/*  <p>This is a paragraph</p>*/}
-      </Greet>
+      {/*</Greet>*/}
       {/*<Greet name='Fegati' heroName='Superman'>*/}
       {/*  <button>Action</button>*/}
       {/*</Greet>*/}
       {/*<Greet name='Bruce' heroName='Wonder Woman'/>*/}
 
-       <Welcome name='Vishwas' heroName='Batman'>
+       {/*<Welcome name='Vishwas' heroName='Batman'>*/}
       {/*  <p>This is a paragraph</p>*/}
-      </Welcome>
+      {/*</Welcome>*/}
       {/*<Welcome name='Fegati' heroName='Superman'>*/}
       {/*  <button>Action</button>*/}
       {/*</Welcome>*/}
