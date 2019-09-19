3f3f2154b743058255575ea1fe07beab659a778d
felixongati <fegati02@gmail.com>
2019-09-19 11:49:07 +0300

    css component styling

--- src/App.js
+++ src/App.js
@@ -15,10 +15,15 @@ import NameList from './components/NameList';
 import StyleSheets from "./components/StyleSheets";
 import Inline from "./components/Inline";
 
+import './appStyles.css'
+import styles from './appStyles.module.css'
+
 function App() {
   return (
     <div className="App">
-        <Inline/>
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
+        {/*<Inline/>*/}
         {/*<StyleSheets primary={false}/>*/}
       {/*<NameList/>*/}
       {/* <UserGreeting/> */}
