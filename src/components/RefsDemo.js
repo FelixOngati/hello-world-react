119f9a70b09920ba6ed225cb1150aef7e220a079
felixongati <fegati02@gmail.com>
2019-10-21 22:20:04 +0300

    refs

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -3,12 +3,31 @@ import React, {Component} from 'react'
 class RefsDemo extends Component{
     constructor(props){
         super(props)
+
+        this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setCbRef = element => {
+            this.cbRef = element
+        }
+    }
+
+    componentDidMount() {
+        if (this.cbRef)
+            this.cbRef.focus()
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
+    }
+
+    clickHandler = () => {
+        alert(this.inputRef.current.value)
     }
 
     render() {
         return(
             <div>
-                <input type="text"/>
+                <input type="text" ref={this.inputRef}/>
+                <input type="text" ref={this.setCbRef}/>
+                <button onClick={this.clickHandler}>Click</button>
             </div>
         )
     }
